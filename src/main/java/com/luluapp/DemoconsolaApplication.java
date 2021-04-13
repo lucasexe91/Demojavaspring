677a07ddda0c2f21ddf0e06ddbd1a2780b044139
Lucas Exequiel Pérez Maidana <lucasexe91@hotmail.com>
2021-04-13 19:58:22 -0300

    demo de programa de consola con matrices

---
+++ src/main/java/com/luluapp/DemoconsolaApplication.java
@@ -0,0 +1,13 @@
+package com.luluapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoconsolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoconsolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luluapp/DemoconsolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luluapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoconsolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
